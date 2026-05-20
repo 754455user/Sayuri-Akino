b0b4653c359b68c0063b937b9d3ed8cf14f392e3
754455user <starboy14441+github@gmail.com>
2026-05-20 11:07:10 +0530

    Add files via upload

--- database/database.py
+++ database/database.py
@@ -1,1 +1,27 @@
+#(©)CodeXBotz - Fixed with Motor (async MongoDB)
 
+import motor.motor_asyncio
+from config import DB_URI, DB_NAME
+
+dbclient = motor.motor_asyncio.AsyncIOMotorClient(DB_URI)
+database = dbclient[DB_NAME]
+user_data = database['users']
+
+async def present_user(user_id: int):
+    found = await user_data.find_one({'_id': user_id})
+    return bool(found)
+
+async def add_user(user_id: int):
+    await user_data.insert_one({'_id': user_id})
+    return
+
+async def full_userbase():
+    user_docs = user_data.find()
+    user_ids = []
+    async for doc in user_docs:
+        user_ids.append(doc['_id'])
+    return user_ids
+
+async def del_user(user_id: int):
+    await user_data.delete_one({'_id': user_id})
+    return
